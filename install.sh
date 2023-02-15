f1f98f27b0c8b25af8f13d045d23e34ffc58d87a
Steven Loh <steven.loh@gmail.com>
2023-02-16 00:38:57 +0800

    Added dotfiles configuration for zsh

--- install.sh
+++ install.sh
@@ -11,10 +11,31 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 # X11 #
 #######
 rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config/X11"
+mkdir -p "$HOME/.config/X11"
+ln -sf "$HOME/dotfiles/X11/xinitrc" "$HOME/.config/X11/.xinitrc"
+ln -sf "$HOME/dotfiles/X11/Xresources" "$HOME/.config/X11/.Xresources"
 
 ######
 # i3 #
 ######
 rm -rf "$HOME/.config/i3"
-ln -s "$HOME/dotfiles/i3" "$HOME/.config/i3"
+mkdir -p "$HOME/.config/i3"
+ln -sf "$HOME/dotfiles/i3/config" "$HOME/.config/i3/config"
+ln -sf "$HOME/dotfiles/i3/i3status.conf" "$HOME/.config/i3/i3status.conf"
+
+#######
+# zsh #
+#######
+
+# .zshenv is intended to be symlinked to $HOME
+rm "$HOME/.zshenv"
+ln -sf "$HOME/dotfiles/zsh/zshenv" "$HOME/.zshenv"
+
+# all other zsh dotfiles are placed in .config/zsh
+rm -rf "$HOME/.config/zsh"
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/zshrc" "$HOME/.config/zsh/.zshrc"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+ln -sf "$HOME/dotfiles/zsh/scripts.sh" "$HOME/.config/zsh/scripts.sh"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh/external"
+
---
+++ zsh/scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+compress() {
+    tar zcvf $1.tar.gz $1
+}
